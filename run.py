7d337b28b3814c63a87e8245e435a141135262a2
AgustinMadygraf <agustin.mtto.madygraf>
2025-08-20 10:18:55 -0300

    Refactor: implement static file handling in FastAPI application and update entry point

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ Path: run.py
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("infrastructure.fastapi.main:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("infrastructure.fastapi.static_server:app", host="0.0.0.0", port=5000, reload=True)
